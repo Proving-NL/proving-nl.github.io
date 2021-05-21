2156065e3d11958a9a05f8fd124b3a7a6ce50c05
Max van Kampen <max.van.kampen@alicon.nl>
2021-05-21 10:18:06 +0200

    Auto commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+$().config({
+  client_id: "2698716-91b0b74c-0286-486a-bf04-add4a41ad5fb",
+  info: {
+    title: "Proving",
+  },
+  client: {
+    servers: [
+      {
+        url: "https://proving.aliconnect.nl/api"
+      }
+    ]
+  },
+})

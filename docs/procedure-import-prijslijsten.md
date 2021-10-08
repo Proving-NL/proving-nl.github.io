5bae5c584523f601d4d4503e37620a7fa75d1a35
Max van Kampen <max.van.kampen@outlook.com>
2021-10-08 17:43:59 +0200

    main

---
+++ docs/procedure-import-prijslijsten.md
@@ -0,0 +1,7 @@
+
+
+
+1. Plaats prijslijst in `Proving BD/prijslijst`
+1. Kopieer naar `archief`
+1. Wijzig naam naar tijdloos / standaard naam
+1. verplaats naar `teimporteren`
---
+++ report/voorraad.php
@@ -0,0 +1,12 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/../vendor/autoload.php");
+$res = aim()->sql_query("EXEC abisingen.api.report_voorraad");
+
+$items=[];
+while ($row = sqlsrv_fetch_object($res)) {
+  $items[]= $row;
+}
+http_response(200, [
+  '@context' => aim()->context,
+  'rows'=>$items,
+]);
